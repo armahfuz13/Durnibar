9f5bf6e9494a1138bde5ebbe936e41852039f98d
armahfuz13 <fuz13edu@gmail.com>
2026-05-06 20:19:24 +0600

    Update page.tsx

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,15 +1,11 @@
 import { MembersPageClient } from './MembersPageClient';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
-import { AdvisorSection } from '@/components/AdvisorSection';
 
 export default function MembersPage() {
   return (
     <main className="min-h-screen bg-[#0f2a40] text-white">
       <Navbar />
-      <div className="pt-16">
-        <AdvisorSection />
-      </div>
       <MembersPageClient />
       <Footer />
     </main>
